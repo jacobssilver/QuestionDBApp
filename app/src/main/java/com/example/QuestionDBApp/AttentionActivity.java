17eb9ce1dd99a220791d982d750945b3197cbe61
jacobssilver <13121813688@163.com>
2019-06-09 16:35:15 +0800

    完善UI

---
+++ app/src/main/java/com/example/QuestionDBApp/AttentionActivity.java
@@ -0,0 +1,12 @@
+package com.example.QuestionDBApp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AttentionActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
