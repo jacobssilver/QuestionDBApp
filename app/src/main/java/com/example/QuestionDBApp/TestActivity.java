574c4589c405f48db86a9d1db8d2140b7ffef0b9
jacobssilver <13121813688@163.com>
2019-06-11 20:05:21 +0800

    继续完善Ui结构

--- app/src/main/java/com/example/QuestionDBApp/TestActivity.java
+++ app/src/main/java/com/example/QuestionDBApp/TestActivity.java
@@ -1,11 +1,11 @@
 package com.example.QuestionDBApp;
 
-import android.provider.Settings;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -21,13 +21,15 @@ import java.util.List;
 public class TestActivity extends AppCompatActivity {
 
     private List<Question> questionList = new ArrayList<>();
+    int i=0;
+
     //fragment
     private FragmentQ1 fragment1;
     private FragmentQ2 fragment2;
     private FragmentQ3 fragment3;
     private FragmentQ4 fragment4;
     private Fragment[] fragments;
-    private  int lastfragment=0;
+    private  int lastfragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,75 +45,120 @@ public class TestActivity extends AppCompatActivity {
             }
         });
         initQuestionList();
+
+        int type = questionList.get(i).getType();
         initfragment();
-        int i=0;
-        switch (questionList.get(i).getType()){
+        lastfragment=type-1;
+
+        switch(type){
             case 1:{
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_question , fragment1).show(fragment1).commit();
+                Bundle bundle = new Bundle();
+                bundle.putInt("QuestionImg",questionList.get(i).getImg());//题目传入fragment
+                fragment1.setArguments(bundle);
                 break;
             }
             case 2:{
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_question , fragment2).show(fragment2).commit();
+                Bundle bundle = new Bundle();
+                bundle.putInt("QuestionImg",questionList.get(i).getImg());//题目传入fragment
+                fragment2.setArguments(bundle);
                 break;
             }
             case 3:{
-                System.out.println("成功进入3");
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_question , fragment3).show(fragment3).commit();
+                Bundle bundle = new Bundle();
+                bundle.putInt("QuestionImg",questionList.get(i).getImg());//题目传入fragment
+                fragment3.setArguments(bundle);
                 break;
             }
             case 4:{
-                break;
-            }
-
-        }
-    }
-
-    public void onitemclick(View v){
-        switch (v.getId()){
-            case R.id.question_list:{
-                Toast.makeText(TestActivity.this,"you have clicked Button3",Toast.LENGTH_SHORT).show();
-                break;
-            }
-            case R.id.button1:{
-                if(lastfragment!=0){
-                    switchFragment(lastfragment,0);
-                    lastfragment = 0;
-                }
-                break;
-            }
-            case R.id.button2:{
-                if(lastfragment!=1){
-                    switchFragment(lastfragment,1);
-                    lastfragment = 1;
-                }
-                break;
-            }
-            case R.id.button3:{
-                if(lastfragment!=2){
-                    switchFragment(lastfragment,2);
-                    lastfragment = 2;
-                }
-                break;
-            }
-            case R.id.button4:{
-                if(lastfragment!=3){
-                    switchFragment(lastfragment,3);
-                    lastfragment = 3;
-                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_question , fragment4).show(fragment4).commit();
+                Bundle bundle = new Bundle();
+                bundle.putInt("QuestionImg",questionList.get(i).getImg());//题目传入fragment
+                fragment4.setArguments(bundle);
                 break;
             }
         }
 
+
+        ImageView go_question = findViewById(R.id.go_question);
+        ImageView back_quesion = findViewById(R.id.back_question);
+        go_question.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(i<questionList.size()-1){
+                    System.out.println("enter go");
+                    i++;
+                    int img = questionList.get(i).getImg();
+                    int type = questionList.get(i).getType();
+                    Bundle bundle = new Bundle();
+                    bundle.putInt("QuestionImg",img);
+                    if(lastfragment!=type-1){
+                        System.out.println("enterswitch");
+                        switchFragment(lastfragment,type-1);
+                        fragments[type].setArguments(bundle);
+                        lastfragment=type-1;
+                    }
+                    else{
+                        System.out.println("enter no_change");
+
+                        getSupportFragmentManager().beginTransaction().show(fragments[lastfragment]).commit();
+                        fragments[lastfragment].setArguments(bundle);
+                    }
+                }
+                else{
+                    Toast toast = Toast.makeText(TestActivity.this,"已是最后一题",Toast.LENGTH_LONG);
+                    toast.setGravity(Gravity.CENTER, 0, 0);
+                    toast.show();
+                }
+            }
+        });
+        back_quesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("enter back");
+                if(i>0){
+                    i--;
+                    System.out.println("enter recycle");
+                    int img = questionList.get(i).getImg();
+                    int type = questionList.get(i).getType();
+                    Bundle bundle = new Bundle();
+                    bundle.putInt("QuestionImg",img);
+                    if(lastfragment!=type-1){
+                        System.out.println("enter switch");
+                        switchFragment(lastfragment,type-1);
+                        fragments[type].setArguments(bundle);
+                        lastfragment=type-1;
+
+                    }
+                    else{
+                        System.out.println("enter back");
+                        getSupportFragmentManager().beginTransaction().show(fragments[lastfragment]).commit();
+                        fragments[lastfragment].setArguments(bundle);
+                        System.out.println("enter no_change");
+                    }
+                }
+                else{
+                    Toast toast = Toast.makeText(TestActivity.this,"已是第一题",Toast.LENGTH_LONG);
+                    toast.setGravity(Gravity.CENTER, 0, 0);
+                    toast.show();
+                }
+            }
+        });
+
+
     }
+
 
     public void initQuestionList(){
         Question question1 = new Question(1,R.drawable.img_a,R.drawable.answer);
         Question question2 = new Question(2,R.drawable.img_b,R.drawable.answer);
         Question question3 = new Question(3,R.drawable.img_c,R.drawable.answer);
 
-        questionList.add(question3);
         questionList.add(question1);
-        questionList.add(question3);
+        questionList.add(question2);
+        questionList.add(question1);
         questionList.add(question2);
         questionList.add(question3);
         questionList.add(question2);
@@ -128,10 +175,13 @@ public class TestActivity extends AppCompatActivity {
     }
     private void switchFragment(int lastfragment, int index){
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.hide(fragments[lastfragment]);
+
         if(!fragments[index].isAdded()){
-            transaction.add(R.id.fragment_question,fragments[index]);
+            transaction.hide(fragments[lastfragment]).add(R.id.fragment_question,fragments[index]).commit();
         }
-        transaction.show(fragments[index]).commitAllowingStateLoss();
+        else{
+            transaction.hide(fragments[lastfragment]).show(fragments[index]).commit();
+        }
+
     }
 }
